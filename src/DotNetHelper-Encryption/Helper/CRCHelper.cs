7d9cc96dbffcd66334e6265c199c7de26bd6cc46
Joseph McNeal Jr <josephmcnealjr@mofade.com>
2019-05-19 18:42:57 -0500

    added CRCHelper

---
+++ src/DotNetHelper-Encryption/Helper/CRCHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetHelper_Encryption.Helper
+{
+    public static class CRCHelper
+    {
+        public static string GetCRC(Stream stream,bool disposeStream)
+        {
+            if (disposeStream)
+            {
+                using (stream)
+                {
+                    var hash = string.Empty;
+                    var bytes = Hashing.Create(HashProvider.CRC32).ComputeHash(stream);
+                    hash = bytes.Aggregate(hash, (current, b) => current + b.ToString("x2").ToLower());
+                    return hash;
+                }
+            }
+            else
+            {
+                var hash = string.Empty;
+                var bytes = Hashing.Create(HashProvider.CRC32).ComputeHash(stream);
+                hash = bytes.Aggregate(hash, (current, b) => current + b.ToString("x2").ToLower());
+                return hash;
+            }
+
+        }
+    }
+}
